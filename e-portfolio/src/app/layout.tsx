a4c2c3b031ebbf836dfa10c15608a5ce97df1555
shelbyneal <81393989+shelbyneal@users.noreply.github.com>
2025-04-05 11:16:39 -0400

    nojekyll

--- e-portfolio/src/app/layout.tsx
+++ e-portfolio/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import './style.css';
+import '../styles/style.css';
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -24,7 +24,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="en">			
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
--- e-portfolio/src/app/page.tsx
+++ e-portfolio/src/app/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 			</Head>
-
 			<div className="container">
 				<header>
 					<div className="wrapper">
